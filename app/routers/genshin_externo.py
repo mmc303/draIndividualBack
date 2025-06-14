1d35923bc7b53492f79a285b09eb07a408010db9
María Martínez <mmc303@inlumine.ual.es>
2025-06-14 02:49:19 +0200

    recoger datos de api externa

--- app/routers/genshin_externo.py
+++ app/routers/genshin_externo.py
@@ -13,3 +13,22 @@ def obtener_personaje_externo(nombre: str):
     except requests.RequestException as e:
         raise HTTPException(status_code=502, detail=f"Error al consultar la API externa: {str(e)}")
 
+@router.get("/externo/weapons/{nombre}")
+def obtener_arma_externa(nombre: str):
+    url = f"https://genshin-db-api.vercel.app/api/v5/weapons?query={nombre}&queryLanguages=spanish&resultLanguage=spanish"
+    try:
+        response = requests.get(url)
+        response.raise_for_status()
+        return response.json()
+    except requests.RequestException as e:
+        raise HTTPException(status_code=502, detail=f"Error al consultar la API externa: {str(e)}")
+    
+@router.get("/externo/artifacts/{nombre}")
+def obtener_artefacto_externo(nombre: str):
+    url = f"https://genshin-db-api.vercel.app/api/v5/artifacts?query={nombre}&queryLanguages=spanish&resultLanguage=spanish"
+    try:
+        response = requests.get(url)
+        response.raise_for_status()
+        return response.json()
+    except requests.RequestException as e:
+        raise HTTPException(status_code=502, detail=f"Error al consultar la API externa: {str(e)}")
