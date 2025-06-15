5a65bea8c1625ff9918aade63c3771d2e4b1f72b
María Martínez <mmc303@inlumine.ual.es>
2025-06-15 19:14:45 +0200

    añadido endpoint material imagen

--- app/routers/genshin_externo.py
+++ app/routers/genshin_externo.py
@@ -32,3 +32,13 @@ def obtener_artefacto_externo(nombre: str):
         return response.json()
     except requests.RequestException as e:
         raise HTTPException(status_code=502, detail=f"Error al consultar la API externa: {str(e)}")
+
+@router.get("/material/{nombre}")
+def obtener_material_externo(nombre: str):
+    url = f"https://genshin-db-api.vercel.app/api/v5/materials?query={nombre}&queryLanguages=spanish&resultLanguage=spanish"
+    try:
+        response = requests.get(url)
+        response.raise_for_status()
+        return response.json()
+    except requests.RequestException as e:
+        raise HTTPException(status_code=502, detail=f"Error al consultar la API externa: {str(e)}")
